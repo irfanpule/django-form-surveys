36d48641693d1d60318be17078deeee0af37d4c0
irfanpule <irfan.biankalang@gmail.com>
2022-02-16 11:55:22 +0700

    context_processor get_master_template

--- surveys/app_settings.py
+++ surveys/app_settings.py
@@ -2,3 +2,5 @@ from django.conf import settings
 
 
 SURVEY_DUPLICATE_ENTRY = settings.SURVEY_DUPLICATE_ENTRY if hasattr(settings, 'SURVEY_DUPLICATE_ENTRY') else False
+SURVEY_MASTER_TEMPLATE = settings.SURVEY_MASTER_TEMPLATE if hasattr(settings,
+                                                                    'SURVEY_MASTER_TEMPLATE') else 'surveys/master.html'
---
+++ surveys/context_processors.py
@@ -0,0 +1,8 @@
+from surveys import app_settings
+
+
+def surveys_context(request):
+    context = {
+        'get_master_template': app_settings.SURVEY_MASTER_TEMPLATE
+    }
+    return context
