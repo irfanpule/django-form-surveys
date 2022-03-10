8a04e9cff207170afde0f54d14217475d77b2407
irfanpule <irfan.biankalang@gmail.com>
2022-03-10 17:07:25 +0700

    refactor config setup.py to detect package

---
+++ djf_surveys/__init__.py
@@ -0,0 +1,3 @@
+__title__ = 'Django Form Surveys'
+__version__ = '1.1'
+__author__ = 'irfanpule'
--- setup.py
+++ setup.py
@@ -1,10 +1,103 @@
 import os
-from setuptools import setup
+import re
+import shutil
+import sys
+from io import open
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
+from setuptools import find_packages, setup
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-setup()
+def read(f):
+    return open(f, 'r', encoding='utf-8').read()
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+def get_packages(package):
+    return [
+        dirpath
+        for dirpath, dirnames, filenames in os.walk(package)
+        if os.path.exists(os.path.join(dirpath, "__init__.py"))
+    ]
+
+
+version = get_version('djf_surveys')
+
+
+if sys.argv[-1] == 'publish':
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("python setup.py sdist bdist_wheel")
+    if os.system("twine check dist/*"):
+        print("twine check failed. Packages might be outdated.")
+        print("Try using `pip install -U twine wheel`.\nExiting.")
+        sys.exit()
+    os.system("twine upload dist/*")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    shutil.rmtree('dist')
+    shutil.rmtree('build')
+    shutil.rmtree('django-form-surveys.egg-info')
+    sys.exit()
+
+
+if sys.argv[-1] == 'build_pre_publish':
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("python setup.py sdist bdist_wheel")
+    if os.system("twine check dist/*"):
+        print("twine check failed. Packages might be outdated.")
+        print("Try using `pip install -U twine wheel`.\nExiting.")
+        sys.exit()
+    # os.system("twine upload dist/*")
+    print("The version now: ", version)
+    sys.exit()
+
+
+setup(
+    name='django-form-surveys',
+    version=version,
+    url='https://irfanpule.github.io/django-form-surveys',
+    license='MIT License',
+    description='A simple Django app to conduct Web-based survey',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    author='irfanpule',
+    author_email='irfan.pule2@gmail.com',
+    packages=get_packages('djf_surveys'),
+    include_package_data=True,
+    install_requires=["django>=2.2", "pytz"],
+    python_requires=">=3.6",
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
