1d05f6225720adc1d15d6dadb3ac222ebb65f5cb
irfanpule <irfan.biankalang@gmail.com>
2022-03-18 13:50:00 +0700

    render star rating on editing form

--- djf_surveys/templatetags/djf_survey_tags.py
+++ djf_surveys/templatetags/djf_survey_tags.py
@@ -1,6 +1,8 @@
 from django.template import Library
-
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 register = Library()
+
 
 @register.filter(name='addclass')
 def addclass(field, class_attr):
@@ -11,3 +13,16 @@ def addclass(field, class_attr):
 def get_id_field(field):
     parse = field.auto_id.split("_")
     return parse[-1]
+
+
+@register.filter(name='create_star')
+def create_star(number):
+    active = int(number)
+    inactive = 5 - active
+    elements = []
+    for _ in range(int(number)):
+        elements.append('<i class ="rating__star rating_active"> </i>')
+    for _ in range(inactive):
+        elements.append('<i class ="rating__star rating_inactive"> </i>')
+
+    return mark_safe(''.join(elements))
