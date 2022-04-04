cd0bba2fb9f6e3b8f55fe80611df16880ef8aa0f
irfanpule <irfan.biankalang@gmail.com>
2022-04-04 16:23:04 +0700

    add max_length validator

--- djf_surveys/app_settings.py
+++ djf_surveys/app_settings.py
@@ -7,3 +7,17 @@ SURVEY_USER_PHOTO_PROFILE = settings.SURVEY_USER_PHOTO_PROFILE if hasattr(settin
 DATE_INPUT_FORMAT = settings.DATE_INPUT_FORMAT if hasattr(settings, 'DATE_INPUT_FORMAT') else \
     ['%Y-%m-%d', '%m/%d/%Y', '%m/%d/%y', '%d/%m/%y', '%d/%m/%Y']
 
+# validators
+field_validators = {
+    'max_length': {
+        'email': 150,
+        'text': 250,
+        'url': 250
+    }
+}
+if hasattr(settings, 'SURVEY_FIELD_VALIDATORS'):
+    max_length = settings.SURVEY_FIELD_VALIDATORS.get('max_length')
+    if max_length:
+        field_validators['max_length'].update(max_length)
+SURVEY_FIELD_VALIDATORS = field_validators
+
