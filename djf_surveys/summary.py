349fa0dad2e70120d040c700cea504499bf85d5b
irfanpule <irfan.pule2@gmail.com>
2022-06-03 07:37:04 +0000

    add class ChartBar, implement for type_field multiple and random colors

--- djf_surveys/summary.py
+++ djf_surveys/summary.py
@@ -1,5 +1,14 @@
+import random
 from djf_surveys.models import TYPE_FIELD, Survey, Question, Answer
 
+
+COLORS = [
+  '#64748b', '#a1a1aa', '#374151', '#78716c', '#d6d3d1', '#fca5a5', '#ef4444', '#7f1d1d',
+  '#fb923c', '#c2410c', '#fcd34d', '#b45309', '#fde047', '#bef264', '#ca8a04', '#65a30d',
+  '#86efac', '#15803d', '#059669', '#a7f3d0', '#14b8a6', '#06b6d4', '#155e75', '#0ea5e9',
+  '#075985', '#3b82f6', '#1e3a8a', '#818cf8', '#a78bfa', '#a855f7', '#6b21a8', '#c026d3',
+  '#db2777', '#fda4af', '#e11d48', '#9f1239'
+]
 
 class ChartJS:
     """
@@ -13,6 +22,7 @@ class ChartJS:
     height = 400
     data = []
     labels = []
+    colors = COLORS
 
     def __init__(self, chart_id: str, chart_name: str, *args, **kwargs):
         self.chart_id = f"djfChart{chart_id}"
@@ -24,6 +34,9 @@ class ChartJS:
     </blocquote>
 </div>
 """
+    
+    def _shake_colors(self):
+        self.colors = random.choices(COLORS, k=len(self.labels))
 
     def _config(self):
         pass
@@ -32,6 +45,7 @@ class ChartJS:
         pass
 
     def render(self):
+        self._shake_colors()
         script = f"""
 {self.element_html}
 <script>
@@ -48,13 +62,6 @@ class ChartJS:
 
 class ChartPie(ChartJS):
     """ this class to generate pie chart"""
-    colors = [
-      '#4dc9f6',
-      '#f67019',
-      '#f53794',
-      '#537bc4',
-      '#acc236'
-    ]
 
     def _config(self):
         script = """
@@ -93,13 +100,47 @@ const data%s = {
         return script % (self.chart_id, self.labels, self.data, self.colors)
 
 
+class ChartBar(ChartJS):
+    """ this class to generate bar chart"""
+
+    def _config(self):
+        script = """
+const config%s = {
+  type: 'bar',
+  data: data%s,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  },
+};
+"""
+        return script % (self.chart_id, self.chart_id)
+
+    def _setup(self):
+        script = """
+const labels = %s;
+const data%s = {
+  labels: labels,
+  datasets: [{
+    label: '%s',
+    data: %s,
+    backgroundColor: %s,
+    borderWidth: 1
+  }]
+};
+"""
+        return script % (self.labels, self.chart_id, self.chart_name, self.data, self.colors)
+
 class SummaryResponse:
 
     def __init__(self, survey: Survey):
         self.survey = survey
     
     def _process_radio_type(self, question: Question) -> str:
-        chart_pie = ChartPie(chart_id=f"chartpie_{question.id}", chart_name=question.label)
+        pie_chart = ChartPie(chart_id=f"chartpie_{question.id}", chart_name=question.label)
         labels = question.choices.split(",")
         data = []
         for label in labels:
@@ -107,20 +148,32 @@ class SummaryResponse:
             count = Answer.objects.filter(question=question, value=clean_label).count()
             data.append(count)
 
-        chart_pie.labels = labels
-        chart_pie.data = data
-        return chart_pie.render()
+        pie_chart.labels = labels
+        pie_chart.data = data
+        return pie_chart.render()
     
     def _process_select_type(self):
         pass
 
-    def _process_multiselect_type(self):
-        pass
+    def _process_multiselect_type(self, question: Question) -> str:
+        bar_chart = ChartBar(chart_id=f"barchart_{question.id}", chart_name=question.label)
+        labels = question.choices.split(",")
+        data = []
+        for label in labels:
+            clean_label = label.strip().replace(' ', '_').lower()
+            count = Answer.objects.filter(question=question, value__contains=clean_label).count()
+            data.append(count)
+
+        bar_chart.labels = labels
+        bar_chart.data = data
+        return bar_chart.render()
 
     def generate(self):
         html_str = []
         for question in self.survey.questions.all():
             if question.type_field == TYPE_FIELD.radio or question.type_field == TYPE_FIELD.select:
                 html_str.append(self._process_radio_type(question))
-                # TODO: 
+            elif question.type_field == TYPE_FIELD.multi_select:
+                html_str.append(self._process_multiselect_type(question))
+
         return " ".join(html_str)
