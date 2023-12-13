ab0302c9eae35c0bf7d4d4bf6229bdf2c019f01f
irfanpule <irfan.pule2@gmail.com>
2023-12-13 10:51:23 +0700

    add default empty select

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -107,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
 # LANGUAGE_CODE = "fr"
-# LANGUAGE_CODE = 'id'
+LANGUAGE_CODE = 'id'
 
 TIME_ZONE = 'Asia/Jakarta'
 
